51ecd70b7e7fd318afb42881616cb06f9a6f7ebc
ABHIRAJ SHARMA <abhirajsharma@ABHIRAJs-MacBook-Air.local>
2024-07-21 03:26:39 +0530

    backend api config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/': {
-        target: 'http://localhost:5001/api/',
+        target: 'https://password-manager-backend-eu27.onrender.com/api/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
